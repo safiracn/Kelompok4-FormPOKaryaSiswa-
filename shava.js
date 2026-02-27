45f109b20ed350db0ef4890e2fb4cec32685b2ea
Daynova Ashavaila Ramadhani <ashavailadaynova@gmail.com>
2026-02-27 22:40:35 +0700

    penambahan validasi angka huruf

--- shava.js
+++ shava.js
@@ -1,16 +1,16 @@
-// DATA PRODUK
+//  DATA PRODUK
 const produkTerpilih = {
     nama: "Meja Belajar Pradipa",
     harga: 100000,
     gambar: "assets/images/meja belajar kotak.jpeg"
 };
 
-//AMBIL ELEMEN
+// AMBIL ELEMEN
 const inputJumlah = document.getElementById('jumlah');
 const displayTotal = document.getElementById('res-total');
 const displayJumlah = document.getElementById('res-jumlah');
 
-// HITUNG HARGA OTOMATIS
+//  HITUNG HARGA OTOMATIS
 inputJumlah.addEventListener('input', function() {
     let qty = parseInt(inputJumlah.value) || 1;
     if (qty < 1) qty = 1;
@@ -21,16 +21,32 @@ inputJumlah.addEventListener('input', function() {
     displayTotal.innerText = "Rp " + total.toLocaleString('id-ID') + ",00";
 });
 
-// VALIDASI 
+// VALIDASI & SUBMIT
 function submitFinal() {
     // Ambil nilai input
     const nama = document.getElementById('nama').value.trim();
     const noTelp = document.getElementById('noTelp').value.trim();
     const alamat = document.getElementById('alamat').value.trim();
+    
+    // Aturan Huruf/Angka
+    const hanyaHuruf = /^[a-zA-Z\s]'+$/;
+    const hanyaAngka = /^[0-9]+$/;
 
     // Cek apakah ada yang kosong
     if (nama === "" || noTelp === "" || alamat === "") {
         alert("Gagal! Semua data harus diisi, tidak boleh ada yang kosong.");
+        return;
+    }
+
+    //  Nama Harus Huruf
+    if (!hanyaHuruf.test(nama)) {
+        alert("Gagal! Nama harus berupa huruf semua, tidak boleh ada angka atau simbol.");
+        return;
+    }
+
+    //  No Telp Harus Angka
+    if (!hanyaAngka.test(noTelp)) {
+        alert("Gagal! No Telepon harus berupa angka.");
         return;
     }
 
